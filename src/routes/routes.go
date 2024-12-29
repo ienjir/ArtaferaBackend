cae7a07073cf5a8ce103658658363920dd65840d
ienjir <info@ienjir.dev>
2024-12-29 20:08:30 +0100

    Cleaned up some code

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ienjir/ArtaferaBackend/src/api/auth"
-	"github.com/ienjir/ArtaferaBackend/src/api/files"
 )
 
 func RegisterRoutes(router *gin.Engine) {
@@ -13,9 +12,4 @@ func RegisterRoutes(router *gin.Engine) {
 		authRoutes.POST("/", auth.LoginHandler)
 	}
 
-	filesRoutes := router.Group("/files")
-	{
-		filesRoutes.POST("/", files.UploadFile)
-		filesRoutes.POST("/d§", files.UploadSingleFile)
-	}
 }
