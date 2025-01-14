5dcdb313c4108ae99a3ecb627f7868c0f6e33d86
ienjir <info@ienjir.dev>
2025-01-14 14:51:12 +0100

    Made a basic Salting+Hashing function

--- src/api/auth/auth.service.go
+++ src/api/auth/auth.service.go
@@ -1,6 +1,68 @@
 package auth
 
-func hashPassword(password string) {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var Argon2IDHash *Argon2idHash
+
+func hashPassword(password string) (*HashSalt, error) {
 	bytePassword := []byte(password)
-	
+
+	hashSalt, err := Argon2IDHash.GenerateHash(bytePassword, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return hashSalt, nil
+}
+
+func GenerateNewArgon2idHash() {
+	hashTimeStr := os.Getenv("HASH_TIME")
+	hashSaltLengthStr := os.Getenv("HASH_SALT_LENGTH")
+	hashMemoryStr := os.Getenv("HASH_MEMORY")
+	hashThreadsStr := os.Getenv("HASH_THREADS")
+	hashKeyLengthStr := os.Getenv("HASH_KEY_LENGTH")
+
+	// Convert to appropriate types
+	hashTime, err := strconv.ParseUint(hashTimeStr, 10, 32)
+	if err != nil {
+		fmt.Printf("Error converting HASH_TIME: %v\n", err)
+		return
+	}
+
+	hashSaltLength, err := strconv.ParseUint(hashSaltLengthStr, 10, 32)
+	if err != nil {
+		fmt.Printf("Error converting HASH_SALT_LENGTH: %v\n", err)
+		return
+	}
+
+	hashMemory, err := strconv.ParseUint(hashMemoryStr, 10, 32)
+	if err != nil {
+		fmt.Printf("Error converting HASH_MEMORY: %v\n", err)
+		return
+	}
+
+	hashThreads, err := strconv.ParseUint(hashThreadsStr, 10, 8)
+	if err != nil {
+		fmt.Printf("Error converting HASH_THREADS: %v\n", err)
+		return
+	}
+
+	hashKeyLength, err := strconv.ParseUint(hashKeyLengthStr, 10, 32)
+	if err != nil {
+		fmt.Printf("Error converting HASH_KEY_LENGTH: %v\n", err)
+		return
+	}
+
+	// Pass converted values to the function
+	Argon2IDHash = NewArgon2idHash(
+		uint32(hashTime),
+		uint32(hashSaltLength),
+		uint32(hashMemory),
+		uint8(hashThreads),
+		uint32(hashKeyLength),
+	)
 }
--- src/main.go
+++ src/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/ienjir/ArtaferaBackend/src/Routes"
 	"github.com/ienjir/ArtaferaBackend/src/api/auth"
 	"github.com/ienjir/ArtaferaBackend/src/database"
 	"github.com/joho/godotenv"
 	"log"
-	"os"
-	"strconv"
 )
-
-var argon2IDHash *auth.Argon2idHash
 
 func main() {
 	router := gin.Default()
@@ -22,7 +17,7 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	generateNewArgon2idHash()
+	auth.GenerateNewArgon2idHash()
 
 	// Set proxies
 	err = router.SetTrustedProxies([]string{"127.0.0.1", "::1"})
@@ -47,52 +42,4 @@ func main() {
 	if err != nil {
 		return
 	}
-}
-
-func generateNewArgon2idHash() {
-	hashTimeStr := os.Getenv("HASH_TIME")
-	hashSaltLengthStr := os.Getenv("HASH_SALT_LENGTH")
-	hashMemoryStr := os.Getenv("HASH_MEMORY")
-	hashThreadsStr := os.Getenv("HASH_THREADS")
-	hashKeyLengthStr := os.Getenv("HASH_KEY_LENGTH")
-
-	// Convert to appropriate types
-	hashTime, err := strconv.ParseUint(hashTimeStr, 10, 32)
-	if err != nil {
-		fmt.Printf("Error converting HASH_TIME: %v\n", err)
-		return
-	}
-
-	hashSaltLength, err := strconv.ParseUint(hashSaltLengthStr, 10, 32)
-	if err != nil {
-		fmt.Printf("Error converting HASH_SALT_LENGTH: %v\n", err)
-		return
-	}
-
-	hashMemory, err := strconv.ParseUint(hashMemoryStr, 10, 32)
-	if err != nil {
-		fmt.Printf("Error converting HASH_MEMORY: %v\n", err)
-		return
-	}
-
-	hashThreads, err := strconv.ParseUint(hashThreadsStr, 10, 8)
-	if err != nil {
-		fmt.Printf("Error converting HASH_THREADS: %v\n", err)
-		return
-	}
-
-	hashKeyLength, err := strconv.ParseUint(hashKeyLengthStr, 10, 32)
-	if err != nil {
-		fmt.Printf("Error converting HASH_KEY_LENGTH: %v\n", err)
-		return
-	}
-
-	// Pass converted values to the function
-	argon2IDHash = auth.NewArgon2idHash(
-		uint32(hashTime),
-		uint32(hashSaltLength),
-		uint32(hashMemory),
-		uint8(hashThreads),
-		uint32(hashKeyLength),
-	)
 }
